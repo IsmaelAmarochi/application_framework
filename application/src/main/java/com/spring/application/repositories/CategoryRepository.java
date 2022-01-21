8a8976b9bad291efc3ce23fedbdbb825612cc4bf
IsmaelAmarochi <61270555+IsmaelAmarochi@users.noreply.github.com>
2022-01-21 17:07:44 +0100

    new start

--- application/src/main/java/com/spring/application/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.application.repositories;
-
-
-import com.spring.application.domain.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-}
--- application/src/main/java/com/spring/application/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.application.repositories;
-
-
-import com.spring.application.domain.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository  extends CrudRepository<Product,Integer> {
-
-}
