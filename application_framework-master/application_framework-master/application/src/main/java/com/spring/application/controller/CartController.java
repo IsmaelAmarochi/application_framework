c8962e816839d50b0b0ed0a98142ead28b29a3af
IsmaelAmarochi <61270555+IsmaelAmarochi@users.noreply.github.com>
2022-01-23 16:53:42 +0100

    end

--- application_framework-master/application_framework-master/application/src/main/java/com/spring/application/controller/CartController.java
+++ application_framework-master/application_framework-master/application/src/main/java/com/spring/application/controller/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CartController {
@@ -39,6 +40,12 @@ public class CartController {
     public String checkout(Model model) {
         model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
         return "checkout";
+    }
+
+    @PostMapping("/orderPlaced")
+    public String orderPlaced() {
+        GlobalData.cart.clear();
+        return "orderPlaced";
     }
 
 }
